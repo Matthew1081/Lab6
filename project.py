608305f1d2bbef85676612f281d727ea27bb4333
Matthew1081 <m.kwiatkowski1078@gmail.com>
2024-07-09 19:32:41 +0200

    Add argument parsing

---
+++ project.py
@@ -0,0 +1,14 @@
+import sys
+
+def main():
+    if len(sys.argv) != 3:
+        print("Usage: program.exe pathFile1.x pathFile2.y")
+        return
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    print(f"Converting {input_file} to {output_file}")
+
+if __name__ == "__main__":
+    main()
